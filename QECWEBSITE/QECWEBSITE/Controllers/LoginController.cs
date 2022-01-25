13ed4ae939ec604686c44c6a5938c65bb2554243
malik_faiq <malikfaiq920@gmail.com>
2022-01-25 21:49:32 +0500

    Adding QEC Project Website

---
+++ QECWEBSITE/QECWEBSITE/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QECWEBSITE.Models;
+
+
+
+namespace QECWEBSITE.Controllers
+{
+    
+    public class LoginController : Controller
+    {
+
+        QECPortalEntities db = new QECPortalEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Login objcheck)
+        {
+            var user = db.Logins.Where(model => model.username == objcheck.username && model.password == objcheck.password).FirstOrDefault();
+            if (user != null)
+            {
+                Session["UserId"] = objcheck.id.ToString();
+                Session["UserName"] = objcheck.username.ToString();
+                TempData["LoginSuccessMessage"] = "<script>alert('Login Successfull !!')</script>";
+                return RedirectToAction("Index", "Maju");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "<script>alert('Username or password is incorrect!!')</script>";
+                return View();
+
+
+            }
+
+        }
+    }
+}
---
+++ QECWEBSITE/QECWEBSITE/Controllers/MajuController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QECWEBSITE.Controllers
+{
+    public class MajuController : Controller
+    {
+        // GET: Maju
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Teacher1()
+        {
+            return View();
+
+        }
+
+        public ActionResult Teacher2()
+        {
+            return View();
+        }
+
+        public ActionResult Teacher3()
+        {
+            return View();
+        }
+
+        public ActionResult Teacher4()
+        {
+            return View();
+        }
+
+
+    }
+}
